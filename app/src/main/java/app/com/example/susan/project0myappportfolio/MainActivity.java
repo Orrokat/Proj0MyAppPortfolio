81ce3ec8547e81373d6b80797aaf69cd654c00dd
Orrokat <susan_a_regan@yahoo.com>
2016-03-27 15:33:09 -0400

    feat: initial commit for Project 0 My App Portfolio

---
+++ app/src/main/java/app/com/example/susan/project0myappportfolio/MainActivity.java
@@ -0,0 +1,106 @@
+package app.com.example.susan.project0myappportfolio;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view, CharSequence text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void launchSpotify(View view) {
+        CharSequence text = "Will launch Spotify Streamer";
+        sendMessage(view, text);
+    }
+
+    public void launchScores(View view) {
+        CharSequence text = "Will launch Scores";
+        sendMessage(view, text);
+    }
+
+    public void launchLibrary(View view) {
+        CharSequence text = "Will launch Library";
+        sendMessage(view, text);
+    }
+
+    public void launchBuildIt(View view) {
+        CharSequence text = "Will launch Build It Bigger";
+        sendMessage(view, text);
+    }
+
+    public void launchReader(View view) {
+        CharSequence text = "Will launch XYZ Reader";
+        sendMessage(view, text);
+    }
+
+    public void launchCapstone(View view) {
+        CharSequence text = "Will launch My Capstone Project";
+        sendMessage(view, text);
+    }
+}
